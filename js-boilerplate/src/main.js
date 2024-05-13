c78be393956858d68908c85139655f40d17ba802
seungdeok <akk9132@naver.com>
2024-05-14 07:49:26 +0900

    refactor: Update boilterplate

--- js-boilerplate/src/main.js
+++ js-boilerplate/src/main.js
@@ -5,5 +5,39 @@ window.addEventListener("load", function () {
 });
 
 function init() {
-  console.log(THREE);
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+
+  camera.position.z = 5;
+
+  render();
+
+  function render() {
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render();
+  }
+
+  window.addEventListener("resize", handleResize);
 }
