dd99bbe80a368e1cc75a59b89a74a55246f4a25a
seungdeok <akk9132@naver.com>
2025-06-16 20:28:28 +0900

    feat: add part3 cube map texture using CubeTextureLoader

--- skybox/src/main.js
+++ skybox/src/main.js
@@ -28,8 +28,40 @@ function init() {
   controls.minDistance = 5;
   controls.maxDistance = 100;
 
-  // https://www.humus.name/index.php?page=Textures
-  const textureLoader = new THREE.TextureLoader().setPath(
+  // // https://www.humus.name/index.php?page=Textures
+  // const textureLoader = new THREE.TextureLoader().setPath(
+  //   "assets/textures/Yokohama/"
+  // );
+
+  // // 카메라 기준
+  // const images = [
+  //   "posx.jpg", //  오른쪽면
+  //   "negx.jpg",
+  //   "posy.jpg", // 윗면
+  //   "negy.jpg",
+  //   "posz.jpg", // 정면
+  //   "negz.jpg",
+  // ];
+
+  // const geometry = new THREE.BoxGeometry(5000, 5000, 5000);
+  // const materials = images.map(
+  //   (image) =>
+  //     new THREE.MeshBasicMaterial({
+  //       map: textureLoader.load(image),
+  //       side: THREE.BackSide,
+  //       /**
+  //        * THREE.FrontSide (기본값): 앞쪽 면만 렌더링
+  //        * THREE.BackSide: 뒤쪽 면만 렌더링(큐브 내부에서 바깥쪽을 보는 것)
+  //        * THREE.DoubleSide: 앞뒤 양쪽 면 모두 렌더링
+  //        */
+  //     })
+  // );
+
+  // const mesh = new THREE.Mesh(geometry, materials);
+
+  // scene.add(mesh);
+
+  const cubeTextureLoader = new THREE.CubeTextureLoader().setPath(
     "assets/textures/Yokohama/"
   );
 
@@ -43,23 +75,9 @@ function init() {
     "negz.jpg",
   ];
 
-  const geometry = new THREE.BoxGeometry(5000, 5000, 5000);
-  const materials = images.map(
-    (image) =>
-      new THREE.MeshBasicMaterial({
-        map: textureLoader.load(image),
-        side: THREE.BackSide,
-        /**
-         * THREE.FrontSide (기본값): 앞쪽 면만 렌더링
-         * THREE.BackSide: 뒤쪽 면만 렌더링(큐브 내부에서 바깥쪽을 보는 것)
-         * THREE.DoubleSide: 앞뒤 양쪽 면 모두 렌더링
-         */
-      })
-  );
+  const cubeTexture = cubeTextureLoader.load(images);
 
-  const mesh = new THREE.Mesh(geometry, materials);
-
-  scene.add(mesh);
+  scene.background = cubeTexture;
 
   render();
 
